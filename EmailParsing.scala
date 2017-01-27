5005ead7976f52007c91bd12e9dbe2d5609371e9
mahesh <mahesh.kndpl@gmail.com>
2017-01-27 13:10:53 +0530

    Extractor and Regex assignment

---
+++ EmailParsing.scala
@@ -0,0 +1,28 @@
+/* 2. Write regular expression for email parsing.
+Description :
+   	val EMAIL = “ regular expression definition”.r
+  	val EMAIL(user, domain) = “knol@knoldus.com”
+      		user = knol
+     		domain = knoldus.com
+*/
+
+import scala.util.matching.Regex
+object EmailParsing extends App{
+    
+      val Email = """([\w\.]+)@([\w\.]+)""".r
+     
+  EmailParsing.emailParser("knol@knoldus.com")
+  println("")
+  EmailParsing.emailParser("mahesh.kndpl@gmail.com")
+  println("")
+  EmailParsing.emailParser("shivangi1015@gmail.com")
+  println("")
+   def emailParser(emailId:String):Unit ={
+    
+        emailId match {
+         case Email(user,domain)=> println(s"User: $user \nDomain: $domain")
+         case _ => println("Invalid email id")
+     }
+ 
+}
+}
